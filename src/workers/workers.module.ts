352d49cb51cf05346411495715d597cf376a61d1
Ferg, Maurício <mauricioferg@gmail.com>
2025-09-19 12:33:52 -0300

    fix: normalize line endings to LF

--- src/workers/workers.module.ts
+++ src/workers/workers.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EmailWorkerService } from './email-worker.service';
 import { ConfigModule } from '@nestjs/config';
+import { EmailModule } from 'src/email/email.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, EmailModule],
   providers: [EmailWorkerService],
   exports: [EmailWorkerService],
 })
